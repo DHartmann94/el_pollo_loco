fd308e41f00c8e08c5e36fe7d5ec4ff5f4399bc1
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-04-12 19:58:40 +0200

    character and chicken spawn

---
+++ models/movable-object.class.js
@@ -0,0 +1,21 @@
+class MovableObject {
+    x = 120;
+    y = 300;
+    img;
+    height = 150;
+    width = 100;
+
+
+    loadImage(path) {
+        this.img = new Image(); // Image existiert bereits in JS
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
---
+++ models/world.class.js
@@ -0,0 +1,24 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    ctx;
+
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
+
+
+    draw() {
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.ctx.drawImage(this.enemies[i].img, this.enemies[i].x, this.enemies[i].y, this.enemies[i].width, this.enemies[i].height);
+        }
+        console.log('world.draw()');
+    }
+}
