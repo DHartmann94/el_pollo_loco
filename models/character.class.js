8f64b6730480ec2db1aa17e4cbb6ebdf0b0347f7
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-04-15 11:16:34 +0200

    background complete

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-
+    y = 175;
+    height = 260;
+    width = 125;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
