1c2e99295eee5c9d05099198b7cdd925731ca0ad
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-04-18 14:40:29 +0200

    invisible wall / sounds

--- models/level.class.js
+++ models/level.class.js
@@ -3,8 +3,9 @@ class Level {
     enemies;
     clouds;
     backgroundObjects;
+    level_end_x = 2200;
 
-    constructor(enemies, clouds, backgroundObjects) { // werden in new Level hierher ausgegeben.
+    constructor(enemies, clouds, backgroundObjects) { // werden aus new Level hierher ausgegeben.
         this.enemies = enemies;
         this.clouds = clouds;
         this.backgroundObjects = backgroundObjects;
--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,6 @@
 class World {
     character = new Character();
-    enemies = level1.enemies; // level1 ist eine Variable/Konstante in der level1.js (dadurch zugriff)
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1; // enemies, clouds, bg in der level 1 Variable.
     canvas; // um canvas in der ganzen klasse nutzen zu können.
     ctx;
     camera_x; // Kamera position verschieben.
@@ -24,12 +22,12 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // löscht alle Bilder im canvas
 
-        this.ctx.translate(this.camera_x, 0); // Kamera wird nach links geschoben, danach werden alle Objecte geladen.
-
-        this.addObjectsToMap(this.backgroundObjects);
-        this.addObjectsToMap(this.clouds);
+        this.ctx.translate(this.camera_x, 0); // Kamera wird nach links geschoben, danach werden alle Objecte geladen. (verschiebt den Ursprung des Koordinatensystems um den x Wert der MO-Klasse)
+        // dadruch sieht es so aus als würde sich nur der Hintergrund bewegen.
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.enemies);
 
         this.ctx.translate(-this.camera_x, 0); // schiebt wenn alle Objecte erstellt wurden den CONTEXT wieder nach rechts.
 
@@ -40,7 +38,7 @@ class World {
         });
     }
 
-    addObjectsToMap(objects) {
+    addObjectsToMap(objects) { // für die arrays
         objects.forEach(object => {
             this.addToMap(object);
         });
@@ -49,13 +47,13 @@ class World {
     addToMap(movableObj) {
         if (movableObj.otherDirection) {
             //wenn true wird das Bild vom Pepe gedreht.
-            this.flipImage(movableObj);
+            this.mirrorImage(movableObj);
         }
 
         this.ctx.drawImage(movableObj.img, movableObj.x, movableObj.y, movableObj.width, movableObj.height);
 
         if (movableObj.otherDirection) {
-            this.flipImageBack(movableObj);
+            this.mirrorImageBack(movableObj);
         }
     }
 
@@ -63,18 +61,18 @@ class World {
      * Rotate the Image if you walk to the left (otherDirection = true).
      * @param {Object} movableObj - The individual classes.
      */
-    flipImage(movableObj) {
+    mirrorImage(movableObj) {
         this.ctx.save(); // einstellung vom context werden gespeichert.
         this.ctx.translate(movableObj.width, 0); // Bild wird gedreht.
         this.ctx.scale(-1, 1); // Bild wird ein Stück verschoben (richtig positioniert)
-        movableObj.x = movableObj.x * -1; // invertiert die x-Achse
+        movableObj.x = movableObj.x * -1; // invertiert die x-Achse (character wird sonst auf die andere seite teleportiert.)
     }
 
     /**
      * Prevents images that should not be mirrored from being mirrored.
      * @param {Object} movableObj - The individual classes.
      */
-    flipImageBack(movableObj) {
+    mirrorImageBack(movableObj) {
         // verhindert das die Bilder die sich nicht spiegeln soll das auch nicht machen.
         this.ctx.restore();
         movableObj.x = movableObj.x * -1; // invertiert die x-Achse
