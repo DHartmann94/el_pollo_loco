8403788bb9ae48fcdcfdc3bdda8c0b2bb7e94373
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-04-20 19:10:56 +0200

    comments

--- levels/level1.js
+++ levels/level1.js
@@ -4,7 +4,7 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Endboss()
-    ], 
+    ],
     [
         new Cloud(),
         new Cloud(),
@@ -18,7 +18,7 @@ const level1 = new Level(
         new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
         new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
         new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-    
+
         new BackgroundObject("img/5_background/layers/air.png", 0),
         new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
         new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
@@ -27,15 +27,15 @@ const level1 = new Level(
         new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
         new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
         new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
-    
-        new BackgroundObject("img/5_background/layers/air.png", 719*2),
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719*2),
-        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719*2),
-        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),
-        new BackgroundObject("img/5_background/layers/air.png", 719*3),
-        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719*3),
-        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719*3),
-        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
+
+        new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
+        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
+        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
+        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
+        new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
+        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
+        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
+        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
     ],
     [
         new Bottle(),
@@ -49,5 +49,5 @@ const level1 = new Level(
         new Coin(),
         new Coin(),
     ],
-    
+
 );
